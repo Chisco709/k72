e600c20db52c11b6ead30ee77139bdaef2a57571
Chisco7089 <juanjosechiscomontoya008@gmail.com>
2025-09-09 17:59:17 -0500

    mejorandotin

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -31,7 +31,7 @@ const HomeBottomText = () => {
         className="uppercase leading-none tracking-tight whitespace-nowrap text-white"
         style={{
           fontFamily: "'Anton', sans-serif",
-          fontSize: '14vw', // 👈 más bajo que antes
+          fontSize: '15vw', // 👈 más bajo que antes
           lineHeight: 1,
           transform: `translateX(${offset}px) scaleX(${scale})`,
           transformOrigin: 'left center'
